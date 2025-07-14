4060f7c5daa68b895ddeabdf0a6e95c72cfba87a
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-07-14 18:02:48 -0300

    Add InteractPatch to handle plot purchasing interactions

---
+++ Patches/InteractPatch.cs
@@ -0,0 +1,41 @@
+using HarmonyLib;
+using ProjectM;
+using ProjectM.Gameplay.Systems;
+using ProjectM.Shared;
+using ScarletCore;
+using ScarletMarket;
+using ScarletMarket.Services;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ScarletQuests.Patches;
+
+[HarmonyPatch(typeof(InteractValidateAndStopSystemServer), nameof(InteractValidateAndStopSystemServer.OnUpdate))]
+internal static class InteractPatch {
+  [HarmonyPrefix]
+  public static void Prefix(InteractValidateAndStopSystemServer __instance) {
+    var query = __instance.__query_195794971_3.ToEntityArray(Allocator.Temp);
+
+    foreach (var entity in query) {
+      if (entity.GetPrefabGuid() != INTERACT_INSPECT) continue;
+      var playerEntity = entity.Read<EntityOwner>().Owner;
+
+      if (playerEntity.IsNull() || !playerEntity.Has<PlayerCharacter>()) continue;
+
+      var player = playerEntity.GetPlayerData();
+
+      if (player == null) continue;
+
+      var inspect = playerEntity.Read<Interactor>().Target;
+      var plot = inspect.Read<Follower>().Followed._Value;
+
+      if (plot.IsNull() || !plot.Exists()) continue;
+
+      if (!plot.IdEquals(PLOT_ID)) {
+        continue;
+      }
+
+      TraderService.TryBuyPlot(player);
+    }
+  }
+}

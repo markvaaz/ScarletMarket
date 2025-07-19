957c36f227288d47ee4ff39a07aad432c5d8a0c1
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-07-18 21:29:11 -0300

    rename Constants.cs to Definitions.cs

--- Definitions.cs
+++ Definitions.cs
@@ -6,35 +6,45 @@ using Unity.Entities;
 namespace ScarletMarket;
 
 internal static class Constants {
-  public const string TRADER_ID = "__ScarletMarket.Trader__";
-  public const string STAND_ID = "__ScarletMarket.Stand__";
-  public const string STORAGE_ID = "__ScarletMarket.Storage__";
-  public const string COFFIN_ID = "__ScarletMarket.Coffin__";
-  public const string PLOT_ID = "__ScarletMarket.Plot__";
-  public const string GHOST_TRADER_ID = "__ScarletMarket.GTrader__";
-  public const string GHOST_STAND_ID = "__ScarletMarket.GStand__";
-  public const string GHOST_STORAGE_ID = "__ScarletMarket.GStorage__";
-  public const string GHOST_COFFIN_ID = "__ScarletMarket.GCoffin__";
-  public const string GHOST_PLOT_ID = "__ScarletMarket.GPlot__";
-  public const string INSPECT_ID = "__ScarletMarket.Inspect__";
-  public const string DISABLED_MESSAGE = "3bf7e066-4e49-4ae4-b7a3-6703b7a15dc1";
-  public const string ENABLED_MESSAGE = "f0c8d1b2-3e4a-4c5b-8f6d-7e8f9a0b1c2d";
-  public const string DONE_MESSAGE = "54d48cbf-6817-42e5-a23f-354ca531c514";
-  public const string CANNOT_DO_MESSAGE = "45e3238f-36c1-427c-b21c-7d50cfbd77bc";
-  public const string CANNOT_MOVE_MESSAGE = "298b546b-1686-414b-a952-09836842bedc";
-  public const string READY_TO_CHANGE_MESSAGE = "57b699b7-482c-4ad1-9ce3-867cd5cca3fb";
-  public const string ALREADY_ASSIGNED_MESSAGE = "d5e62c6c-751f-4629-bfc5-459fd79ea41a";
-  public const string PRIVATE_MESSAGE = "80e97474-e56f-4356-bc7d-698a807ac714";
-  public const string FREE_MESSAGE = "fc3179c1-3f18-4044-b207-c1c148fb1cd4";
-  public const string OPEN_MESSAGE = "4ab8d098-2c0c-4719-bf0f-852522d2b424";
-  public const string CLOSE_MESSAGE = "9b97e97d-7d95-4900-af81-1f8457c25182";
   public const int COFFIN_HEIGHT = 223;
-  public const float PLOT_RADIUS = 2.5f;
+  public const float PLOT_RADIUS = 2.3f;
   public static readonly PrefabGUID NEUTRAL_FACTION = new(-1430861195);
   public static readonly PrefabGUID BLOCK_SLOT_ITEM = new(1488205677); // -696770536
   public static readonly PrefabGUID SCT_PREFAB = new(-1404311249);
   public static readonly PrefabGUID INTERACT_INSPECT = new(222103866);
-  public static NativeParallelHashMap<PrefabGUID, Entity> PrefabGuidToEntityMap = GameSystems.PrefabCollectionSystem._PrefabGuidToEntityMap;
+}
+
+internal static class GameData {
+  public static NativeParallelHashMap<PrefabGUID, Entity> PrefabGuidToEntityMap => GameSystems.PrefabCollectionSystem._PrefabGuidToEntityMap;
+}
+
+
+internal static class SCTMessages {
+  public const string Disabled = "3bf7e066-4e49-4ae4-b7a3-6703b7a15dc1";
+  public const string Enabled = "f0c8d1b2-3e4a-4c5b-8f6d-7e8f9a0b1c2d";
+  public const string Done = "54d48cbf-6817-42e5-a23f-354ca531c514";
+  public const string CannotDo = "45e3238f-36c1-427c-b21c-7d50cfbd77bc";
+  public const string CannotMove = "298b546b-1686-414b-a952-09836842bedc";
+  public const string ReadyToChange = "57b699b7-482c-4ad1-9ce3-867cd5cca3fb";
+  public const string AlreadyAssigned = "d5e62c6c-751f-4629-bfc5-459fd79ea41a";
+  public const string Private = "80e97474-e56f-4356-bc7d-698a807ac714";
+  public const string Free = "fc3179c1-3f18-4044-b207-c1c148fb1cd4";
+  public const string Open = "4ab8d098-2c0c-4719-bf0f-852522d2b424";
+  public const string Close = "9b97e97d-7d95-4900-af81-1f8457c25182";
+}
+
+internal static class Ids {
+  public const string Trader = "__ScarletMarket.Trader__";
+  public const string Stand = "__ScarletMarket.Stand__";
+  public const string Storage = "__ScarletMarket.Storage__";
+  public const string Coffin = "__ScarletMarket.Coffin__";
+  public const string Plot = "__ScarletMarket.Plot__";
+  public const string GhostTrader = "__ScarletMarket.GTrader__";
+  public const string GhostStand = "__ScarletMarket.GStand__";
+  public const string GhostStorage = "__ScarletMarket.GStorage__";
+  public const string GhostCoffin = "__ScarletMarket.GCoffin__";
+  public const string GhostPlot = "__ScarletMarket.GPlot__";
+  public const string Inspect = "__ScarletMarket.Inspect__";
 }
 
 internal static class Spawnable {
